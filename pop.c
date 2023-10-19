fcad95f2a2fccf9aa9d956acbacc1359e17becb6
Ami Manye <amimanyeaglago@gmail.com>
2023-10-19 12:23:03 +0000

    function that removes the top element from the stack

---
+++ pop.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * pop - removes the top element off the stack
+ * @head: head / top of the stack
+ * @line_num: line number at which stack is empty
+ */
+void pop(stack_t **head, int line_num)
+{
+	if (head == NULL)
+		stack_empty_error(line_num);
+	else
+	{
+		*head = (*head)->next;
+	}
+}
