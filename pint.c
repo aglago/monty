7fdff3afe70b38bcfb3d79b7a3cfc128b2ee4df9
Ami Manye <amimanyeaglago@gmail.com>
2023-10-19 12:22:32 +0000

    function that prints the top element of a stack

---
+++ pint.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * pint - prints value at the top of the stack
+ * @head: head / top of the stack
+ * @line_num: line number at stack is empty
+ */
+void pint(stack_t *head, int line_num)
+{
+	if (head == NULL)
+		stack_empty_error(line_num);
+	else
+	{
+		printf("%d\n", head->n);
+	}
+}
