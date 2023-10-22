73cb1c82033d97453b15cbd0ca89c8687eb3808f
Ami Manye <amimanyeaglago@gmail.com>
2023-10-22 12:17:37 +0000

    function that multiplies between top two elements in the stack

---
+++ mul.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+ * mul - muls first two elements on stack
+ * @head: head of the dll
+ * @line_num: the line number of the error instruction
+ */
+
+void mul(stack_t **head, unsigned int line_num)
+{
+	int muli;
+
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*head)->n == 0)
+	{
+		fprintf(stderr, "L%u: mulision by zero\n", line_num);
+                exit(EXIT_FAILURE);
+	}
+
+	muli = (*head)->next->n * (*head)->n;
+	(*head)->next->n = muli;
+
+
+	*head = (*head)->next;
+	(*head)->prev = NULL;
+	free_dll(head);
+}
