299497bf1b5ea0c6d896555ee0c86c2ccf43a24c
Ami Manye <amimanyeaglago@gmail.com>
2023-10-22 14:57:01 +0000

    function that prints a string

---
+++ pstr.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * pstr - prints character related to the integer
+ * at the top of the stack
+ * @head: head / top of the stack
+ * @line_num: line number at stack is empty
+ */
+void pstr(stack_t **head, unsigned int line_num)
+{
+	stack_t *curr_top = *head;/* Adjusted to use a double pointer*/
+	(void) line_num; /* We need it declared beause of our function*/
+
+	if (*head == NULL)
+	{
+		printf("\n");
+	}
+
+	while (curr_top)
+	{
+		if ((*head)->n <= 0 || (*head)->n > 127)
+		{
+			return;
+		}
+		printf("%c\n", curr_top->n);
+		curr_top = curr_top->next;
+	}
+}
