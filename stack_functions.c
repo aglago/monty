71334adf8df22938c56253507848a87ebf7e51a7
Ami Manye <amimanyeaglago@gmail.com>
2023-10-22 12:43:35 +0000

    added a comma after adding the call for mod

--- stack_functions.c
+++ stack_functions.c
@@ -23,7 +23,7 @@ void (*operate(char *opcode))(stack_t **stack, unsigned int line_number)
 		{"sub", sub},
 		{"div", divide},
 		{"mul", mul},
-		{"mod", mod}
+		{"mod", mod},
 		{NULL, NULL}
 	};
 
