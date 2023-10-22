1b237390a25deab82b02a355c6adcb128a005dc5
Ami Manye <amimanyeaglago@gmail.com>
2023-10-22 12:40:42 +0000

    added prototype of mod function

--- monty.h
+++ monty.h
@@ -71,5 +71,6 @@ void free_dll(stack_t **head);
 void sub(stack_t **head, unsigned int line_num);
 void divide(stack_t **head, unsigned int line_num);
 void mul(stack_t **head, unsigned int line_num);
+void mod(stack_t **head, unsigned int line_num);
 
 #endif
